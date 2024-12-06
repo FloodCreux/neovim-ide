d4731c27a6fbf02a864b303a65f1e0a61b60c8c4
Mike Flood <flood.mike@gmail.com>
2024-12-06 11:06:05 -0600

    update hm module

--- lib/hm.nix
+++ lib/hm.nix
@@ -1,3 +1,4 @@
+# Home Manager module
 {
   config,
   pkgs,
@@ -11,16 +12,53 @@ let
 in
 with lib;
 {
+  meta.maintainers = [ maintainers.gvolpe ];
+
   options.programs.neovim-ide = {
-    enable = mkEnableOption "Neovim with LSP enabled fo Scala, Rust, and more.";
+    enable = mkEnableOption "NeoVim with LSP enabled for Scala, Haskell, Rust and more.";
 
-    settings = mkOption { };
-
-    finalPackage = mkOption { };
-
-    config = mkIf cfg.enable {
-      home.packages = [ set.neovim ];
-      programs.neovim-ide.finalPackage = set.neovim;
+    settings = mkOption {
+      type = types.attrsOf types.anything;
+      default = { };
+      example = literalExpression ''
+        {
+          vim.viAlias = false;
+          vim.vimAlias = true;
+          vim.lsp = {
+            enable = true;
+            formatOnSave = true;
+            lightbulb.enable = true;
+            lspsaga.enable = false;
+            nvimCodeActionMenu.enable = true;
+            trouble.enable = true;
+            lspSignature.enable = true;
+            rust.enable = false;
+            nix = true;
+            dhall = true;
+            elm = true;
+            haskell = true;
+            scala = true;
+            sql = true;
+            python = false;
+            clang = false;
+            ts = false;
+            go = false;
+          };
+        }
+      '';
+      description = "Attribute set of neovim-ide preferences.";
     };
+
+    finalPackage = mkOption {
+      type = types.package;
+      visible = false;
+      readOnly = true;
+      description = "The NeoVim package including as configured by the HM module";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ set.neovim ];
+    programs.neovim-ide.finalPackage = set.neovim;
   };
 }
