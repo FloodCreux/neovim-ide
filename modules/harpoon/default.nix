cfc0bd98855985d53c8280f20e3f149d9615acf5
Mike Flood <mike@MacBookPro.lan>
2024-12-08 21:18:30 -0600

    fix harpoon

--- modules/harpoon/default.nix
+++ modules/harpoon/default.nix
@@ -29,40 +29,39 @@ in
       harpoon:setup()
 
       ${writeIf tele.enable ''
-        local themes = require("telescope.themes")
-        local hm_actions = require("telescope._extensions.harpoon_marks.actions")
+        local tele_conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
 
-        vim.keymap.set(
-          "n",
-          "<leader>hl",
-          function()
-            require("telescope").extensions.harpoon.marks(themes.get_dropdown({
-              previewer = false,
-              layout_config = { width = 0.6 },
-              path_display = { truncate = 10 },
-              attach_mappings = function(_, map)
-                map("i", "<c-d>", h,_actions.delete_mark_selections)
-                map("n", "<c-d>", h,_actions.delete_mark_selections)
-                return true
-              end,
-            }))
-          end,
-          { desc = "List" }
-        )
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = tele_conf.file_previewer({}),
+                sorter = tele_conf.generic_sorter({}),
+            }):find()
+        end
+
+        vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+            { desc = "Open harpoon window" })
       ''}
 
       ${writeIf keys.enable ''
         wk.add({
           {"<leader>h", group = "Harpoon"},
-          {"<leader>ha", "<cmd>lua require('harpoon'):list():add()<CR>", desc = "Add"},
-          {"<leader>hd", "<cmd>lua require('harpoon'):list():remove()<CR>", desc = "Del"},
-          {"<leader>ht", "<cmd>lua require('harpoon.ui'):toggle_quick_menu(harpoon:list())<CR>", desc = "Toggle Menu"},
-          {"<leader>hh", "<cmd>lua require('harpoon'):list():select(1)<CR>", desc = "Go To 1"},
-          {"<leader>hj", "<cmd>lua require('harpoon'):list():select(2)<CR>", desc = "Go To 2"},
-          {"<leader>hk", "<cmd>lua require('harpoon'):list():select(3)<CR>", desc = "Go To 3"},
-          {"<leader>hl", "<cmd>lua require('harpoon'):list():select(4)<CR>", desc = "Go To 4"},
-          {"<leader>hn", "<cmd>lua require('harpoon'):list():prev()<CR>", desc = "Go To Prev"},
-          {"<leader>hp", "<cmd>lua require('harpoon'):list():next()<CR>", desc = "Go To Next"},
+          {"<leader>ha", "<cmd> lua require('harpoon'):list():add()<CR>", desc = "Add"},
+          {"<leader>hd", "<cmd> lua require('harpoon'):list():remove()<CR>", desc = "Del"},
+          {"<leader>ht", "<cmd> lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<CR>", desc = "Toggle Menu"},
+          {"<leader>hh", "<cmd> lua require('harpoon'):list():select(1)<CR>", desc = "Go To 1"},
+          {"<leader>hj", "<cmd> lua require('harpoon'):list():select(2)<CR>", desc = "Go To 2"},
+          {"<leader>hk", "<cmd> lua require('harpoon'):list():select(3)<CR>", desc = "Go To 3"},
+          {"<leader>hl", "<cmd> lua require('harpoon'):list():select(4)<CR>", desc = "Go To 4"},
+          {"<leader>hn", "<cmd> lua require('harpoon'):list():prev()<CR>", desc = "Go To Prev"},
+          {"<leader>hp", "<cmd> lua require('harpoon'):list():next()<CR>", desc = "Go To Next"},
         })
       ''}
     '';
